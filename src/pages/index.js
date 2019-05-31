caf000edc0a40ffa5e3a0491c9e03690dc7e4c2d
mikepuglisi <mikepuglisi@gmail.com>
2019-05-31 12:16:05 -0400

    Update

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,17 +5,28 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+const IndexPage = ({location}) => {
+  const params = new URLSearchParams(location.search);
+  const color = params.get('color');
+  return (
+      <Layout>
+        <SEO title="Home" />
+        <h1>Hi people</h1>
+        { color ?
+          <p style={{color: color}}>
+            I SHOULD BE THE COLOR {color} in production mode even after hitting CTRL+F5 (hard referesh)
+          </p> :
+          <p>
+            No Color was passed. Add ?color=blue to URL and hit CTRL+F5 to ensure a hard refresh
+          </p>
+        }
+
+        <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+          <Image />
+        </div>
+        <Link to="/page-2/">Go to page 2</Link>
+      </Layout>
+    )
+  }
 
 export default IndexPage
